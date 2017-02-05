2be13dff906dbd3539d51337301309bd12c86033
amazingpikachu_37 <ian.gs@outlook.com>
2017-02-05 13:54:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -573,7 +573,6 @@ var ColoredUsernames;
         'MewDP': '#FFFF33',
         'DaveTheDave_': '#00BFFF',
         'Luigi86101': '#006400'
-	'thetiredlemur': '#464942'
     };
     // Possible colors for other users
     var colors = ['Blue', 'Coral', 'DodgerBlue', 'SpringGreen', 'YellowGreen', 'Green', 'OrangeRed', 'Red', 'GoldenRod', 'CadetBlue', 'SeaGreen', 'Chocolate', 'BlueViolet', 'Firebrick'];
